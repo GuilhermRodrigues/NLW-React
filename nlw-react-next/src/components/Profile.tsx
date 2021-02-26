0ee07a4b71a7da22470f44d44d3da93e87d7ed4d
GuilhermRodrigues <guilherme_rodrigues@alu.ufc.br>
2021-02-26 11:21:23 -0300

    NLW - conclusao do componente Profile

--- nlw-react-next/src/components/Profile.tsx
+++ nlw-react-next/src/components/Profile.tsx
@@ -6,7 +6,10 @@ export function Profile() {
             <img src="https://github.com/GuilhermRodrigues.png" alt="Guilherme Rodridgues"/>
             <div>
                 <strong>Guilherme Rodrigues</strong>
-                <p>Level 1</p>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level 1
+                </p>
             </div>
 
         </div>
